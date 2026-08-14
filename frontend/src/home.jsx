347d5aab7d8822ca949d19548722929b861314aa
RonaldSzeto <rs9316@nyu.edu>
2026-08-14 01:26:41 -0400

    changed style to an ascii look

--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -6,21 +6,24 @@ function Home() {
       <h1>Algorithm Visualizer</h1>
       <p>Select an algorithm below to visualize how it works step-by-step:</p>
 
-      <div className="sorting_name_box">
+      <div className="sorting_name_box">  
         <Link to="/bubble-sort" className="algo-card">
+        <div className="card-body">
           <div className="card-image-box">
             <img 
               src="/floating-soap-bubbles-in-various-sizes-isolated-on-white-free-png.webp"
               className="card-img">
            </img>
            </div>
-          <h2>Bubble Sort</h2>
+          <h2 className = "name"> Bubble Sort</h2>
           <p className = "subtitle"> mmm bubbles</p> 
+          </div>
         </Link>
+        </div>
         
-        {/* We can easily add Quick Sort, Merge Sort, etc. here later! */}
-      </div>
+        {/* other sorts */}
     </div>
+    
   );
 }
 
